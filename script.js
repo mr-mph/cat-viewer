51065df86c2c1b9f4861735348c8259939506c27
mr-mph <88854453+mr-mph@users.noreply.github.com>
2022-09-01 22:05:57 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const cat_image_el = document.getElementById('image');
+const next_button_el = document.getElementById('next');
+const previous_button_el = document.getElementById('previous');
+const cats = [];
+
+async function nextCatImage() {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search');
+    const image = await response.json();
+    const image_url = await image[0].url
+    cats.push(cat_image_el.src);
+    cat_image_el.src = image_url;
+    console.log(cats);
+    if (cats.length > 1) {
+        previous_button_el.disabled = false;
+    }
+    return image_url;
+}
+
+async function lastCatImage() {
+    if (cats.length > 1) {
+        cat_image_el.src = cats.pop();
+        console.log(cats)
+        if (cats.length <= 1) {
+            previous_button_el.disabled = true;
+        }
+    }
+
+}
+
+nextCatImage();
+
+
+next_button_el.addEventListener('click', nextCatImage);
+previous_button_el.addEventListener('click', lastCatImage);
+
+document.addEventListener('keydown', function(e) {
+    key = e.key
+    if (key === 'ArrowLeft') {
+        lastCatImage();
+    } else if (key === 'ArrowRight') {
+        nextCatImage();
+    }
+
+});
+
+
